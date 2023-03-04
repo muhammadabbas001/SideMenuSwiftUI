ee31062d7c72ede2014910a1db9aabc74cc2ebbd
Muhammad Abbas <abbas@vigoroustechnologies.com>
2023-03-04 12:20:00 -0600

    Initial Commit

---
+++ SideMenuSwiftUI/SideMenuSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuSwiftUIApp.swift
+//  SideMenuSwiftUI
+//
+//  Created by Zeeshan Suleman on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
